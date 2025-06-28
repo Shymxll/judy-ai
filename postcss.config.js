473344ca38ed144b62eae77aed67020ee71b7575
Shamkhal Huseynzade <shamkhalhuseynzade26@gmail.com>
2025-06-28 15:50:32 +0300

    Update dependencies: Bump postcss to version 8.5.6 and tailwindcss to version 4.1.11 in package.json and package-lock.json for improved functionality and compatibility.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
-const config = {
+// postcss.config.js
+module.exports = {
   plugins: {
-    "@tailwindcss/postcss": {},
-    "autoprefixer": {}
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
-export default config;
